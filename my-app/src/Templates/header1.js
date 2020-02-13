8a4e91a88cbb88f36f986a7ae12f9bbe0ef817cb
basssowmi <sowmiya.aero.96@gmail.com>
2020-02-13 18:54:46 +0530

    nav-header changes

--- my-app/src/Templates/header1.js
+++ my-app/src/Templates/header1.js
@@ -195,16 +195,16 @@ export default class Headerdata extends Component {
         <nav  className="navbar navbar-expand-lg navbar-light bg-info">
         
         <Container fluid={true}>
-           <button className="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span className="navbar-toggler-icon"></span>
-           </button>
+        <button className="navbar-toggler bg-white" type="button"   onClick={ this.toggleMenu } aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
             <Col md="3" xs="6" sm="4" lg="2">
            <img style={{width: "100%"}} src={Logoimg}></img>
            </Col>
            
           
            <Col md="9" xs="12" sm="8" lg="10">
-           <div className="collapse navbar-collapse float-right"  id="navbarSupportedContent">
+           <div className={"float-right collapse navbar-collapse " + show}  >
             
           
              
